de68fb06edb0cec62f61802523bbd5fdd647170e
Cao Nguyen <cao.nguyen@platform.inc>
2024-08-10 17:53:44 +0700

    Update notion ai

---
+++ server/server.js
@@ -0,0 +1,236 @@
+require('dotenv').config(); // Load .env file
+
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Client, iteratePaginatedAPI } = require('@notionhq/client');
+
+const app = express();
+const port = 3001;
+var cors = require('cors')
+
+app.use(cors())
+
+// Middleware to parse JSON data
+app.use(bodyParser.json());
+
+// Route to handle POST requests
+app.post('/data', (req, res) => {
+	const data = req.body;
+
+	console.log('Received data:', data);
+
+	// Respond with a success message
+	res.status(200).json({ message: 'Data received successfully', receivedData: data });
+});
+
+
+
+app.post('/access.token', async (req, res) => {
+
+	const data = req.body;
+	// encode in base 64
+	const encoded = Buffer.from(`${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_NOTION_SECRET}`).toString("base64");
+
+	const response = await fetch("https://api.notion.com/v1/oauth/token", {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			Authorization: `Basic ${encoded}`,
+		},
+		body: JSON.stringify({
+			grant_type: "authorization_code",
+			code: data.code,
+			redirect_uri: process.env.REACT_APP_REDIRECT_URI
+		}),
+	}).then(e => e.json());
+
+	console.log('response', response);
+	console.log(`${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_REDIRECT_URI}`)
+
+
+	res.status(200).json({ access_token: response.access_token });
+});
+
+
+app.post('/get.pages', async (req, res) => {
+
+	const notion = new Client({ auth: req.body.access_token });
+
+	const pages = await getAllPagesAndSubpages(req.body.page_id, notion);
+
+	console.log('pages', pages);
+	res.status(200).json({ pages});
+});
+
+
+// Start the server
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+
+
+// Take rich text array from a block child that supports rich text and return the plain text.
+// Note: All rich text objects include a plain_text field.
+const getPlainTextFromRichText = richText => {
+	return richText.map(t => t.plain_text).join("")
+	// Note: A page mention will return "Undefined" as the page name if the page has not been shared with the integration. See: https://developers.notion.com/reference/block#mention
+}
+
+// Use the source URL and optional caption from media blocks (file, video, etc.)
+const getMediaSourceText = block => {
+	let source, caption
+
+	if (block[block.type].external) {
+		source = block[block.type].external.url
+	} else if (block[block.type].file) {
+		source = block[block.type].file.url
+	} else if (block[block.type].url) {
+		source = block[block.type].url
+	} else {
+		source = "[Missing case for media block types]: " + block.type
+	}
+	// If there's a caption, return it with the source
+	if (block[block.type].caption.length) {
+		caption = getPlainTextFromRichText(block[block.type].caption)
+		return caption + ": " + source
+	}
+	// If no caption, just return the source URL
+	return source
+}
+
+// Get the plain text from any block type supported by the public API.
+const getTextFromBlock = block => {
+	let text
+
+	// Get rich text from blocks that support it
+	if (block[block.type].rich_text) {
+		// This will be an empty string if it's an empty line.
+		text = getPlainTextFromRichText(block[block.type].rich_text)
+	}
+	// Get text for block types that don't have rich text
+	else {
+		switch (block.type) {
+			case "unsupported":
+				// The public API does not support all block types yet
+				text = "[Unsupported block type]"
+				break
+			case "bookmark":
+				text = block.bookmark.url
+				break
+			case "child_database":
+				text = block.child_database.title
+				// Use "Query a database" endpoint to get db rows: https://developers.notion.com/reference/post-database-query
+				// Use "Retrieve a database" endpoint to get additional properties: https://developers.notion.com/reference/retrieve-a-database
+				break
+			case "child_page":
+				text = block.child_page.title
+				break
+			case "embed":
+			case "video":
+			case "file":
+			case "image":
+			case "pdf":
+				text = getMediaSourceText(block)
+				break
+			case "equation":
+				text = block.equation.expression
+				break
+			case "link_preview":
+				text = block.link_preview.url
+				break
+			case "synced_block":
+				// Provides ID for block it's synced with.
+				text = block.synced_block.synced_from
+					? "This block is synced with a block with the following ID: " +
+					block.synced_block.synced_from[block.synced_block.synced_from.type]
+					: "Source sync block that another blocked is synced with."
+				break
+			case "table":
+				// Only contains table properties.
+				// Fetch children blocks for more details.
+				text = "Table width: " + block.table.table_width
+				break
+			case "table_of_contents":
+				// Does not include text from ToC; just the color
+				text = "ToC color: " + block.table_of_contents.color
+				break
+			case "breadcrumb":
+			case "column_list":
+			case "divider":
+				text = "No text available"
+				break
+			default:
+				text = "[Needs case added]"
+				break
+		}
+	}
+	// Blocks with the has_children property will require fetching the child blocks. (Not included in this example.)
+	// e.g. nested bulleted lists
+	if (block.has_children) {
+		// For now, we'll just flag there are children blocks.
+		text = text + " (Has children)"
+	}
+	// Includes block type for readability. Update formatting as needed.
+	return block.type + ": " + text
+}
+
+async function retrieveBlockChildren(id, notion) {
+	console.log("Retrieving blocks (async)...")
+	const blocks = []
+
+	// Use iteratePaginatedAPI helper function to get all blocks first-level blocks on the page
+	for await (const block of iteratePaginatedAPI(notion.blocks.children.list, {
+		block_id: id, // A page ID can be passed as a block ID: https://developers.notion.com/docs/working-with-page-content#modeling-content-as-blocks
+	})) {
+		blocks.push(block)
+	}
+
+	return blocks
+}
+
+const getBlockText = (blocks, notion) => {
+	console.log("Displaying blocks:")
+
+	let res = ''
+	for (let i = 0; i < blocks.length; i++) {
+		 text = getTextFromBlock(blocks[i], notion)
+		// Print plain text for each block.
+		res = res + "\n" + text;
+	}
+
+	console.log(res);
+	return res;
+}
+
+
+
+async function getAllPagesAndSubpages(pageId, notion) {
+	const pages = [];
+	const traversePage = async (id, notion) => {
+		const blocks = await retrieveBlockChildren(id, notion);
+
+		pages.push({
+			id: id,
+			content: getBlockText(blocks, notion)
+		});
+		for (const block of blocks) {
+			if (block.type === 'child_page') {
+				await traversePage(block.id, notion); // Recursively fetch subpages
+			} 
+			
+			// else if (block.type === 'link_to_page') {
+			// 	pages.push({
+			// 		id: block.id,
+			// 		title: block.link_to_page.page_id,
+			// 	});
+			// 	await traversePage(block.link_to_page.page_id); // Recursively fetch subpages
+			// }
+		}
+	};
+	await traversePage(pageId, notion);
+	return pages;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,163 @@
-import React from 'react';
+//@ts-ignore
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
+import OpenAI from 'openai';
 
+import { getEmbedding as originGetEmbedding, EmbeddingIndex } from 'client-vector-search';
+import { pipeline, env } from "@xenova/transformers";
+
+// Disable local models
+env.allowLocalModels = false;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	// const openai = axios.create({
+	// 	baseURL: 'https://api.openai.com/v1',
+	// 	headers: {
+	// 		'Content-Type': 'application/json',
+	// 		'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_KEY}`,
+	// 	},
+	// });
+
+	// const getOpenAIResponse = async (prompt: any) => {
+	// 	const response = await openai.post('/completions', {
+	// 		model: 'text-davinci-003',
+	// 		prompt: prompt,
+	// 		max_tokens: 100,
+	// 	});
+	// 	return response.data;
+	// };
+
+	const getAccessToken = () => {
+		const urlParams = new URLSearchParams(window.location.search);
+		const code = urlParams.get('code');
+		if (code) {
+			// Exchange code for access token
+			exchangeCodeForToken(code);
+		}
+	};
+
+
+	const exchangeCodeForToken = async (code: string) => {
+
+		axios.post('http://localhost:3001/access.token', {
+			code
+		})
+			.then(response => {
+				console.log('Response from server:', response.data);
+				if (response.data.access_token) {
+					localStorage.setItem('access_token', response.data.access_token);
+					window.location.href = `${process.env.REACT_APP_REDIRECT_URI}`;
+				}
+				window.location.href = `${process.env.REACT_APP_REDIRECT_URI}`;
+			})
+			.catch(error => {
+				console.error('Error:', error);
+			});
+	};
+
+
+	const getPages = async () => {
+		axios.post('http://localhost:3001/get.pages', {
+			access_token: localStorage.getItem('access_token'),
+			page_id: '2be24753722945c897620fe4ef8663c3'
+		})
+			.then(response => {
+				console.log('Response from server:', response.data);
+				if (response.data.pages) {
+					localStorage.setItem('pages', JSON.stringify(response.data.pages));
+				}
+
+			})
+			.catch(error => {
+				console.error('Error:', error);
+			});
+	}
+
+
+	const authorize = () => {
+		window.location.href = `https://api.notion.com/v1/oauth/authorize?owner=user&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.REACT_APP_REDIRECT_URI as string)}&response_type=code`;
+	};
+
+
+	const getInsignts = async() => {
+
+		console.log(process.env.REACT_APP_OPENAI_KEY,)
+		const openai = new OpenAI({
+			apiKey: process.env.REACT_APP_OPENAI_KEY,
+			dangerouslyAllowBrowser: true
+		});
+
+		const pages = localStorage.getItem('pages');
+		
+		const chatCompletion = await openai.embeddings.create({
+			model: 'text-embedding-3-large',
+			input: ['A happy moment', 'I am sad.'],
+		});
+
+		console.log(chatCompletion);
+	};
+
+
+	const getEmbedding = async(text: string) => {
+		return originGetEmbedding(text, 7, undefined, 'Xenova/gte-small');
+	}
+
+	const loadPages = async() => {
+		const pages = localStorage.getItem('pages');
+
+		let res =  [
+			{ id: 2, name: "Banana", embedding: await getEmbedding("Banana") },
+			{ id: 3, name: "Cheddar", embedding: await getEmbedding("Cheddar")},
+			{ id: 4, name: "Space", embedding: await getEmbedding("Space")},
+			{ id: 5, name: "database", embedding: await getEmbedding("database")},
+		];
+
+		let data: any[] = [];
+		// res.map(async (e: any) => {
+		// 	if (!e.embedding){
+		// 		e.embedding = await getEmbedding(e.content);
+		// 	}
+
+		// 	data.push(e);
+		// })
+		
+		console.log('res', data);
+		const index = new EmbeddingIndex(data); // Creates an ind
+
+		const index_pages = await index.getAllObjectsFromIndexedDB();
+		console.log('index_pages', index_pages);
+
+		// specify the storage type
+		await index.saveIndex('indexedDB');
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<button onClick={getAccessToken}>Get access token</button>
+				<button onClick={authorize}>Login notion</button>
+
+				<button onClick={getPages}>Get Pages</button>
+				<button onClick={getInsignts}>Get Insights</button>
+
+				<button onClick={loadPages}>Get Pages In Storages</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
